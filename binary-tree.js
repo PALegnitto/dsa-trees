c6ff35f39b4754e8e0db6d8abd45fac9a4ca9c9a
Phill Legnitto <palegnitto@gmail.com>
2022-09-21 12:30:45 -0700

    completed excercise

--- binary-tree.js
+++ binary-tree.js
@@ -16,8 +16,30 @@ class BinaryTree {
   /** minDepth(): return the minimum depth of the tree -- that is,
    * the length of the shortest path from the root to a leaf. */
 
-  minDepth(node = this.root) {
+   minDepth(node = this.root) {
 
+    if(node === null) return 0;
+
+    let minTotal = Infinity;
+
+    function recursive(current, total = 1) {
+      if (current.left === null && current.right === null) {
+        if (total < minTotal) {
+          minTotal = total
+        }
+      }
+      if (current.left) {
+
+        recursive(current.left, total + 1)
+      }
+      if (current.right) {
+
+        recursive(current.right, total + 1)
+      }
+    }
+
+    recursive(node)
+    return minTotal
   }
 
   /** maxDepth(): return the maximum depth of the tree -- that is,
@@ -25,12 +47,54 @@ class BinaryTree {
 
   maxDepth(node = this.root) {
 
+    if(node === null) return 0;
+
+    let maxTotal = -Infinity;
+
+    function recursive(current, total = 1) {
+      if (current.left === null && current.right === null) {
+        if (total > maxTotal) {
+          maxTotal = total
+        }
+      }
+      if (current.left) {
+        recursive(current.left, total + 1)
+      }
+      if (current.right) {
+        recursive(current.right, total + 1)
+      }
+    }
+
+    recursive(node)
+    return maxTotal
+
   }
 
   /** nextLarger(lowerBound): return the smallest value in the tree
    * which is larger than lowerBound. Return null if no such value exists. */
 
   nextLarger(lowerBound, node = this.root) {
+
+    if (node === null) return null;
+
+    let smallestVal = Infinity;
+
+    let toVisitStack = [this.root];
+
+    while (toVisitStack.length) {
+      let current = toVisitStack.pop();
+      if (current.val > lowerBound && current.val < smallestVal) {
+        smallestVal = current.val;
+      }
+
+      if (current.right){
+        toVisitStack.push(current.right)
+      }
+      if (current.left){
+        toVisitStack.push(current.left)
+      }
+    }
+    return smallestVal === Infinity ? null : smallestVal;
 
   }
 
--- tree.js
+++ tree.js
@@ -15,19 +15,63 @@ class Tree {
   /** sumValues(): add up all of the values in the tree. */
 
   sumValues() {
-    
+    if (this.root === null) return 0;
+
+    let sum = 0;
+    let toVisitStack = [this.root];
+
+    while (toVisitStack.length) {
+      let current = toVisitStack.pop();
+      sum += current.val;
+
+      for (let child of current.children){
+        toVisitStack.push(child);
+      }
+    }
+    return sum;
   }
 
   /** countEvens(): count all of the nodes in the tree with even values. */
 
   countEvens() {
+    if (this.root === null) return 0;
 
+    let total = 0;
+    let toVisitStack = [this.root];
+
+    while (toVisitStack.length) {
+      let current = toVisitStack.pop();
+      if (current.val % 2 === 0) {
+        total++;
+      }
+
+      for (let child of current.children){
+        toVisitStack.push(child);
+      }
+    }
+    return total;
   }
 
   /** numGreater(lowerBound): return a count of the number of nodes
    * whose value is greater than lowerBound. */
 
   numGreater(lowerBound) {
+    if (this.root === null) return 0;
+    
+    let total = 0;
+    let toVisitStack = [this.root];
+
+    while (toVisitStack.length) {
+      let current = toVisitStack.pop();
+      if (current.val > lowerBound) {
+        total++;
+      }
+
+      for (let child of current.children){
+        toVisitStack.push(child);
+      }
+    }
+    return total;
 
   }
 }
